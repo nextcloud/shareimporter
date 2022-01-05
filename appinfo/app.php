4a868b763bd7833b7ac83585fed8208cdcb9d821
Arthur Schiwon <blizzz@arthur-schiwon.de>
2022-01-05 23:09:50 +0100

    fix various deprecations
    Signed-off-by: Arthur Schiwon <blizzz@arthur-schiwon.de>

--- appinfo/app.php
+++
@@ -1,5 +0,0 @@
-<?php
-namespace OCA\ShareImporter\AppInfo;
-
-$app = new Application();
-$app->getContainer()->query('UserHooks')->register();
--- lib/AppInfo/Application.php
+++ lib/AppInfo/Application.php
@@ -1,27 +1,39 @@
 <?php
+
 namespace OCA\ShareImporter\AppInfo;
 
-use \OCP\AppFramework\App;
-use \OCA\ShareImporter\Hooks\UserHooks;
+use OCA\ShareImporter\Hooks\UserHooks;
+use OCP\AppFramework\App;
+use OCP\AppFramework\Bootstrap\IBootContext;
+use OCP\AppFramework\Bootstrap\IBootstrap;
+use OCP\AppFramework\Bootstrap\IRegistrationContext;
+use OCP\AppFramework\IAppContainer;
 
-class Application extends App {
+class Application extends App implements IBootstrap {
+	public const APPID = 'shareimporter';
 
-    public function __construct(array $urlParams=array()){
-        parent::__construct('shareimporter', $urlParams);
+	public function __construct() {
+		parent::__construct(self::APPID);
+	}
 
-        $container = $this->getContainer();
+	public function register(IRegistrationContext $context): void {
+		$context->registerService(UserHooks::class, function (IAppContainer  $c) {
+			return new UserHooks(
+				$c->get('AppName'),
+				$c->get('ServerContainer')->getUserManager(),
+				$c->get('ServerContainer')->getLogger(),
+				$c->get('ServerContainer')->getUserGlobalStoragesService(),
+				$c->get('ServerContainer')->getGlobalStoragesService(),
+				$c->get('ServerContainer')->getStoragesBackendService(),
+				$c->get('ServerContainer')->getConfig(),
+				$c->get('ServerContainer')->getHTTPClientService()
+			);
+		});
+	}
 
-        $container->registerService('UserHooks', function($c) {
-            return new UserHooks(
-                $c->query('AppName'),
-                $c->query('ServerContainer')->getUserManager(),
-                $c->query('ServerContainer')->getLogger(),
-                $c->query('ServerContainer')->getUserGlobalStoragesService(),
-                $c->query('ServerContainer')->getGlobalStoragesService(),
-                $c->query('ServerContainer')->getStoragesBackendService(),
-                $c->query('ServerContainer')->getConfig(),
-                $c->query('ServerContainer')->getHTTPClientService()
-            );
-        });
-    }
+	public function boot(IBootContext $context): void {
+		/** @var UserHooks $listener */
+		$listener = $context->getAppContainer()->get(UserHooks::class);
+		$listener->register();
+	}
 }
